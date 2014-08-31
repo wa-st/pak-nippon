86d428d5561f33cb02b797e0e2c0c583ed21dea1
wa <ttrawa@gmail.com>
2014-09-01 02:55:12 +0900

    食品工場入れ替え

--- pak.nippon/setup.bat
+++ pak.nippon/setup.bat
@@ -158,6 +158,7 @@ set TARGET_DIR=R:\simutrans\pak
 @del %TARGET_DIR%\factory.brewery.pak
 @del %TARGET_DIR%\factory.cannery.pak
 @del %TARGET_DIR%\factory.dairy.pak
+@del %TARGET_DIR%\factory.food_processing_plant.pak
 @del %TARGET_DIR%\factory.scrap_yard.pak
 @del %TARGET_DIR%\factory.supermarket.pak
 @del %TARGET_DIR%\factory.thermisches_Solarkraftwerk.pak
