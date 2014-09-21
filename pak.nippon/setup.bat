907521046fc25eace86e10933b465f4d2b753855
wa <ttrawa@gmail.com>
2014-09-22 02:22:16 +0900

    米農家追加

--- pak.nippon/setup.bat
+++ pak.nippon/setup.bat
@@ -162,6 +162,7 @@ set TARGET_DIR=R:\simutrans\pak
 @del %TARGET_DIR%\factory.food_processing_plant.pak
 @del %TARGET_DIR%\factory.grain_mill.pak
 @del %TARGET_DIR%\factory.grain_windmill.pak
+@del %TARGET_DIR%\factory.old_farm.pak
 @del %TARGET_DIR%\factory.scrap_yard.pak
 @del %TARGET_DIR%\factory.supermarket.pak
 @del %TARGET_DIR%\factory.thermisches_Solarkraftwerk.pak
